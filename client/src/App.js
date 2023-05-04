eb58eb62e73d20f24d6faafe42b7eec5d71fcf4f
herohoro <mtwd.office.25@gmail.com>
2023-05-05 01:13:12 +0900

    5-40_Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import AuthLayout from "./components/layout/AuthLayout";
+import Login from "./pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { blue } from "@mui/material/colors";
 
 function App() {
-  return <div className="App"></div>;
+  const theme = createTheme({
+    palette: { primary: blue },
+  });
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route>
+            <Route path="/" element={<AuthLayout />}>
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
