ec8b5ad8ba453283fff6096065d3ba9ee4b4cf96
herohoro <mtwd.office.25@gmail.com>
2023-05-13 03:48:47 +0900

    5-63_JWTリダイレクト

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -3,6 +3,7 @@ const { default: axiosClient } = require("./axiosClient");
 const authApi = {
   register: (params) => axiosClient.post("auth/register", params),
   login: (params) => axiosClient.post("auth/login", params),
+  verifyToke: () => axiosClient.post("auth/verify-token"),
 };
 
 export default authApi;
